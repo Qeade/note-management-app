c12788ab979021c27f29e04c5d9144186b33adb5
Rostik <ralforov@gmail.com>
2025-08-19 22:06:14 +0300

    Create models

---
+++ backend/src/models/notes.ts
@@ -0,0 +1,49 @@
+export interface Note {
+    id: number;
+    text: string;
+}
+
+let notes: Note[] = [];
+
+export const getAll = (): Note[] => notes;
+
+export const getById = (id: number): Note | undefined => {
+    return notes.find(note => note.id === id);
+};
+
+
+export const create = (text: string): Note => {
+    const newNote: Note = {
+        id: notes.length > 0 ? notes[notes.length - 1].id + 1 : 1,
+        text
+    };
+
+    notes.push(newNote);
+    return newNote;
+};
+
+
+export const update = (id: number, newText: string): Note | undefined => {
+    const note = notes.find(note => note.id === id);
+
+    if (!note) {
+        return undefined; // якщо не знайшли
+    }
+
+    note.text = newText;
+    return note;
+};
+
+export const remove = (id: number): Note | undefined => {
+    const index = notes.findIndex(note => note.id === id);
+
+    if (index === -1) return undefined;
+
+    const deletedNote = notes[index];
+
+    notes.splice(index, 1);
+
+    return deletedNote;
+};
+
+
