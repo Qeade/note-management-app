480bec22d19859b76dfb2b463fe8d373f2880816
Rostik <ralforov@gmail.com>
2025-08-19 22:06:03 +0300

    Create controllers

---
+++ backend/src/controllers/notes.ts
@@ -0,0 +1,83 @@
+import { Request, Response } from "express";
+import * as Notes from "../models/notes";
+
+export const getAll = (req: Request, res: Response): void => {
+    const allNotes = Notes.getAll();
+    res.json(allNotes);
+};
+
+export const getById = (req: Request, res: Response): void => {
+    const id = Number(req.params.id);
+
+    if (isNaN(id)) {
+        res.status(400).json({ error: "Id must be a number" });
+        return;
+    }
+
+    const note = Notes.getById(id);
+
+    if (!note) {
+        res.status(404).json({ error: "Note not found" });
+        return;
+    }
+
+    res.status(200).json(note);
+};
+
+
+export const create = (req: Request, res: Response): void => {
+    const { text } = req.body;
+
+    if (!text) {
+        res.status(400).json({ error: "Text is required" });
+        return;
+    }
+
+    const newNote = Notes.create(text);
+    res.status(201).json(newNote);
+};
+
+export const update = (req: Request, res: Response): void => {
+    const id = Number(req.params.id);
+
+    if (isNaN(id)) {
+        res.status(400).json({ error: "Id must be a number" });
+        return;
+    }
+
+    const { newText } = req.body;
+
+    if (!newText) {
+        res.status(400).json({ error: "Text is required" });
+        return;
+    }
+
+    const updatedNote = Notes.update(id, newText);
+
+    if (!updatedNote) {
+        res.status(404).json({ error: "Note not found" });
+        return;
+    }
+
+    res.status(200).json(updatedNote);
+};
+
+
+
+export const remove = (req: Request, res: Response): void => {
+    const id = Number(req.params.id);
+
+    if (isNaN(id)) {
+        res.status(400).json({ error: "Id must be a number" });
+        return;
+    }
+
+    const deletedNote = Notes.remove(id);
+
+    if (!deletedNote) {
+        res.status(404).json({ error: "Note not found" });
+        return;
+    }
+
+    res.status(200).json({ message: "Note deleted successfully", note: deletedNote });
+};
